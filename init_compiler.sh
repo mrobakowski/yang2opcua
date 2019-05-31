2187120a1dd9960aed02871aa0361fed89f426b0
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-05-31 17:29:08 +0200

    generate typedefs

---
+++ init_compiler.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt install mono-devel mono-complete
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+pushd $(pwd)
+
+cd $DIR/model_compiler
+wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+mono nuget.exe install -OutputDirectory packages ModelCompiler/packages.config
+
+# On older systems you may need to use xbuild instead of msbuild
+xbuild "ModelCompiler Solution.sln" /p:TargetFrameworkVersion="v4.5"
+
+popd
